22ca49050dd6a1b97558dd82d81cefc3fce0e163
hungtt <hungtddgcd201514@fpt.edu.vn>
2022-02-15 04:18:48 +0700

    up

---
+++ index.js
@@ -0,0 +1,39 @@
+// create a rest api for stitch
+const lib = require('./lib');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+app.use(function (req, res, next) { //allow cross origin requests
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+	res.header("Access-Control-Max-Age", "3600");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+	next();
+});
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/getPatternData', async (req, res) => {
+    let data = await lib.getPatternData();
+    res.send(JSON.stringify({'data':{'databasesListNames':data[0], 'collectionListNames':data[1]}}))
+})
+app.get('/getDataByQuery', async (req, res) => {
+    let data = await lib.getDataByQuery(req.query.databaseName, req.query.collectionName, req.query.query);
+    res.send(JSON.stringify({'data':data}))
+})
+app.get('/getAllData', async (req, res) => {
+    let data = await lib.getAllData(req.query.databaseName, req.query.collectionName);
+    res.send(JSON.stringify({'data':data}))
+})
+app.get('/convert2CSV', async (req, res) => {
+    let data = await lib.convert2CSV(req.query.data);
+    res.send(JSON.stringify({'data':data}))
+})
+app.get('/addData', async (req, res) => {
+    let data = await lib.addData(req.query.databaseName, req.query.collectionName, req.query.data);
+    res.send(JSON.stringify({'data':data}))
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ lib.js
@@ -0,0 +1,88 @@
+const {MongoClient} = require('mongodb');
+const uri = 'mongodb+srv://dev_phongnt:dataversion123@fdvc.tlxdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const client = new MongoClient(uri);
+async function listDatabases(client){
+    let databasesList = await client.db().admin().listDatabases();
+    let databasesListNames = databasesList.databases.map(db => db.name);
+    return databasesListNames;
+};
+async function getPatternData() {
+    try {
+        await client.connect();
+        let databasesListNames = await listDatabases(client);
+        let collectionListNames = await Promise.all(databasesListNames.map(async (nameDatabase) =>  await client.db(nameDatabase).listCollections().toArray().then(collections => {
+                if(nameDatabase != 'admin' && nameDatabase != 'local'){
+                    let listName = collections.map(collection => collection.name);
+                    return listName
+                }else{
+                    return []
+                }
+            })
+
+        ))
+        return [databasesListNames, collectionListNames]
+    } catch (e) {
+        console.error(e);
+    }
+    finally {
+        console.log('closing connection');
+        await client.close();
+    }
+}
+async function getDataByQuery(databaseName, collectionName, query){
+    try {
+        await client.connect();
+        let data = await client.db(databaseName).collection(collectionName).find(query).toArray();
+        return data
+    } catch (e) {
+        console.error(e);
+    }
+    finally {
+        console.log('closing connection');
+        await client.close();
+    }
+}
+function convert2CSV(data){
+    let csv = '';
+    for(let i = 0; i < data.length; i++){
+        for(let key in data[i]){
+            csv += data[i][key] + ','
+        }
+        csv += '\n'
+    }
+    return csv
+}
+async function getAllData(databaseName,collectionName){
+    try {
+        await client.connect();
+        // limit 200 documents
+        let data = await client.db(databaseName).collection(collectionName).find().limit(200).toArray();
+        return data
+    } catch (e) {
+        console.error(e);
+    }
+    finally {
+        console.log('closing connection');
+        await client.close();
+    }
+}
+async function addData(databaseName, collectionName, data){
+    try {
+        await client.connect();
+        await client.db(databaseName).collection(collectionName).insertOne(data);
+        return 'success'
+    } catch (e) {
+        console.error(e);
+    }
+    finally {
+        console.log('closing connection');
+        await client.close();
+    }
+}
+module.exports = {
+    getPatternData,
+    convert2CSV,
+    getDataByQuery,
+    getAllData,
+    addData
+}
